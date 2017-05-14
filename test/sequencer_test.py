acc8d509c02d8e2919b30b4f7c2be4105a018a8a
Oscar <oscarbc1996@gmail.com>
2017-05-14 08:58:52 +0200

    update sequencer test

--- test/sequencer_test.py
+++ test/sequencer_test.py
@@ -23,6 +23,8 @@ class SequencerTest(unittest.TestCase):
         peer0 = self.host.spawn('peer0', Sequencer)
         peer0.attach(monitor, group)
 
+        self.assertEqual(('local://local:6666/peer0', False), peer0.get_sequencer_url())
+
         peer0.multicast('1')
         peer0.multicast('2')
 
@@ -32,6 +34,22 @@ class SequencerTest(unittest.TestCase):
 
         self.assertEqual([], peer0.get_wait_queue())
 
+        peer1 = self.host.spawn('peer1', Sequencer)
+        peer1.attach(monitor, group)
+
+        sleep(1)
+        peer0.leave_group()
+        sleep(1)
+
+        peer2 = self.host.spawn('peer2', Sequencer)
+        peer2.attach(monitor, group)
+
+        sleep(2)
+
+        self.assertEqual(('local://local:6666/peer2', False), peer1.get_sequencer_url())
+        self.assertEqual(('local://local:6666/peer2', False), peer2.get_sequencer_url())
+
+        monitor.stop_monitoring()
 
     def tearDown(self):
         unittest.TestCase.tearDown(self)
