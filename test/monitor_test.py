9fb2bc49d54710c3b6af97fa7423ad89453d2518
Oscar <oscarbc1996@gmail.com>
2017-05-15 23:48:27 +0200

    updated monitor test

--- test/monitor_test.py
+++ test/monitor_test.py
@@ -1,6 +1,7 @@
 import unittest
-from pyactor.context import set_context, create_host, shutdown
+from pyactor.context import set_context, create_host, shutdown, sleep
 from groupcast.monitor import Monitor
+import sys
 
 
 class MonitorTest(unittest.TestCase):
@@ -12,8 +13,17 @@ class MonitorTest(unittest.TestCase):
     def test_monitor(self):
         monitor = self.host.spawn('monitor', Monitor)
 
-        monitor.to_print("test")
+        monitor.start_monitoring()
 
+        monitor.to_print("test")
+        sleep(1)
+        self.assertEqual(sys.stdout.getvalue().strip(), 'test')
+
+        monitor.print_table()
+        sleep(1)
+        self.assertEqual(sys.stdout.getvalue().strip(), 'test')
+
+        monitor.stop_monitoring()
 
     def tearDown(self):
         shutdown()
--- test/peer_test.py
+++ test/peer_test.py
@@ -23,6 +23,9 @@ class PeerTest(unittest.TestCase):
         peer0 = self.host.spawn('peer0', Lamport)
         peer0.attach_group(group)
 
+        peer1 = self.host.spawn('peer1', Lamport)
+        peer1.attach_group(group)
+
         sleep(0.5)
         peer0.multicast('1')
         peer0.multicast('2')
@@ -33,18 +36,24 @@ class PeerTest(unittest.TestCase):
 
         self.assertEqual([], peer0.get_wait_queue())
 
-        peer1 = self.host.spawn('peer1', Lamport)
-        peer1.attach_group(group)
-
         sleep(1)
         peer0.leave_group()
         sleep(1)
 
         peer2 = self.host.spawn('peer2', Lamport)
-        # peer2.attach_group(group)
+        peer2.attach_group(group)
         sleep(0.5)
         peer3 = self.host.spawn('peer3', Lamport)
-        # peer3.attach_group(group)
+        peer3.attach_group(group)
+
+        sleep(0.5)
+        peer1.multicast('3')
+        peer1.multicast('4')
+        sleep(0.5)
+        self.assertEqual(['3', '4'], peer3.get_messages())
+        sleep(0.5)
+        self.assertEqual(['1', '2', '3', '4'], peer1.get_messages())
+
 
     def test_sequencer(self):
         group = self.host.spawn('group', Group)
@@ -55,6 +64,10 @@ class PeerTest(unittest.TestCase):
 
         self.assertEqual(('local://local:6666/peer0', False), peer0.get_sequencer_url())
 
+        peer1 = self.host.spawn('peer1', Sequencer)
+        peer1.attach_group(group)
+        self.assertEqual(('local://local:6666/peer0', False), peer1.get_sequencer_url())
+
         peer0.multicast('1')
         peer0.multicast('2')
 
@@ -64,26 +77,21 @@ class PeerTest(unittest.TestCase):
 
         self.assertEqual([], peer0.get_wait_queue())
 
-        sleep(0.5)
-        peer1 = self.host.spawn('peer1', Sequencer)
-        sleep(0.5)
-        # peer1.attach_group(group)
-
         sleep(1)
         peer0.leave_group()
         sleep(1)
 
         peer2 = self.host.spawn('peer2', Sequencer)
-        # peer2.attach_group(group)
+        peer2.attach_group(group)
         sleep(0.5)
         peer3 = self.host.spawn('peer3', Sequencer)
-        # peer3.attach_group(group)
+        peer3.attach_group(group)
 
         sleep(1)
 
-        # self.assertEqual(('local://local:6666/peer2', False), peer1.get_sequencer_url())
-        # self.assertEqual(('local://local:6666/peer2', False), peer2.get_sequencer_url())
-        # self.assertEqual(('local://local:6666/peer2', False), peer3.get_sequencer_url())
+        self.assertEqual(('local://local:6666/peer2', False), peer1.get_sequencer_url())
+        self.assertEqual(('local://local:6666/peer2', False), peer2.get_sequencer_url())
+        self.assertEqual(('local://local:6666/peer2', False), peer3.get_sequencer_url())
 
     def tearDown(self):
         shutdown()
